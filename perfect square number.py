b6303e99ff8e003950595d68fcb5a0ca15a643bf
42732058 <126454772+42732058@users.noreply.github.com>
2023-03-02 14:58:07 +0530

    Add files via upload

---
+++ perfect square number.py
@@ -0,0 +1,10 @@
+import math
+
+# Taking the number input from user
+num = int(input("Enter the Number: "))
+
+root = math.sqrt(num)
+if int(root + 0.5) ** 2 == num:
+    print(num, "is a perfect square.")
+else:
+    print(num, "is not a perfect square.")
